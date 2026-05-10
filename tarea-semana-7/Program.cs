5dc6b8497a7a4d5469b16b97a6123bd206aecaa8
gerald <gallardorojasgeraldantonio@gmail.com>
2026-05-10 12:17:25 -0500

    Agregar trabajo01 en Program.cs

--- tarea-semana-7/Program.cs
+++ tarea-semana-7/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,8 +10,39 @@ namespace tarea_semana_7
     internal class Program
     {
         static void Main(string[] args)
+       
         {
-            
+            trabajo01();
         }
+        static void trabajo01()
+        {
+            int numero;
+            int mayor = 0;
+            int contador = 0;
+
+            Console.Write("Ingrese un número (0 para salir): ");
+            numero = int.Parse(Console.ReadLine());
+
+            while (numero != 0)
+            {
+                if (numero > mayor)
+                {
+                    mayor = numero;
+                }
+                else
+                {
+                    contador++;
+                }
+
+                Console.Write("Ingrese un número (0 para salir): ");
+                numero = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("El número mayor es: " + mayor);
+            Console.WriteLine("Cantidad de números menores: " + contador);
+        }
+        
     }
+    
 }
+    
